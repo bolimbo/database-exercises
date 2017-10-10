f1b06b9c584a4fdd277c338ce657568f63101e95
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-10 13:54:18 -0500

    used insert into and values , then migrated to migration file then seed to database

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+Use codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+  VALUES ('Michael Jackson','Thriller', 1982, 4.7 ,'Pop, Rock, R&B'),
+    ('AC/DC', 'Back in BLack', 1980, 26.1, 'Hard Rock'),
+    ('Pink Floyd', 'The Dark Side of the Moon', 1973,24.2, 'Progressive rock'),
+    ('Whitney Houston','The Bodyguard', 1992,27.4, 'R&B, Soul, Pop, Soundtrack ')
