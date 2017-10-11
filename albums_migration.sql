5c457a4cf9d2c0a78ee403975955ff73b0c96913
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-11 14:25:57 -0500

    update albums set where
    select *
    from

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS albums (
   id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
   artist       VARCHAR(150) NOT NULL,
   name         VARCHAR(150) NOT NULL,
-  release_date YEAR(4)      NOT NULL,
+  release_date SMALLINT      NOT NULL,
   sales        FLOAT        NOT NULL,
   genre        TEXT,
   PRIMARY KEY (id)
