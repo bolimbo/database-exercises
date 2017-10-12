42a365c1a64367cf9023b80f2f2eaad845cdcc98
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-12 11:00:35 -0500

    complete exercise fpr where lesson using like, not like or,and

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,20 +1,29 @@
 use employees;
 
 
-select *
+select first_name, last_name, gender
 from employees
-where first_name = 'Irena'
+where (first_name = 'Irena'
       or first_name = 'Vidya'
-      or first_name = 'Maya';
+      or first_name = 'Maya')
+and gender = 'M';
 
 
-select *
-from employees
-where last_name LIKE 'E%';
 
 select *
 from employees
-where hire_date = '90';
+where last_name LIKE 'E%'
+OR last_name like '%E';
+
+select *
+from employees
+where last_name LIKE 'E%'
+      and last_name like '%E';
+
+select *
+from employees
+where hire_date BETWEEN '1990-01-01' and '1999-12-31'
+and birth_date like '%-12-25';
 
 select *
 from employees
@@ -22,4 +31,7 @@ where birth_date LIKE '%-12%-25%';
 
 select *
 from employees
-where last_name LIKE '%q%';
+where last_name LIKE '%q%'
+and last_name NOT like '%qu%';
+
+
